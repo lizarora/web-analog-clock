a3b3bb2f1851e6b01d8c2ebcdc488251ca930041
Liza Arora <128188840+lizarora@users.noreply.github.com>
2024-07-31 18:56:47 -0500

    different time zones working with edge cases of hand rotations

--- index.js
+++ index.js
@@ -1,17 +1,47 @@
-setInterval(() => {
+let selectedTimeZoneDifference = 0;
 
+function currentTime() {
     date = new Date();
 
-    hourTime = date.getHours();
-    minuteTime = date.getMinutes();
-    secondTime = date.getSeconds();
+    let hourDiff = Math.floor(selectedTimeZoneDifference);
+    let minDiff = (selectedTimeZoneDifference - hourDiff) *60
+    let hourTime = date.getHours() + hourDiff;
+    let minuteTime = date.getMinutes() + minDiff;
+    let secondTime = date.getSeconds();
 
-    hourRotation = 30*hourTime + minuteTime/2;
-    minuteRotation = 6*minuteTime;
-    secondRotation = 6*secondTime;
+    if (hourTime >= 24) {
+        hourTime -= 24;
+    } else if (hourTime < 0) {
+        hourTime += 24;
+    }
+
+    hourRotation = 30 * hourTime + minuteTime / 2;
+    minuteRotation = 6 * minuteTime;
+    secondRotation = 6 * secondTime;
 
     document.getElementById('hour').style.transform = `rotate(${hourRotation}deg)`;
     document.getElementById('minute').style.transform = `rotate(${minuteRotation}deg)`;
     document.getElementById('second').style.transform = `rotate(${secondRotation}deg)`;
-}, 1000);
+}
+
+document.getElementById('timezone1').addEventListener('click', () => {
+    selectedTimeZoneDifference = 1; // EST 
+    currentTime();
+});
+
+document.getElementById('timezone2').addEventListener('click', () => {
+    selectedTimeZoneDifference = -2; // PST 
+    currentTime();
+});
+
+document.getElementById('timezone3').addEventListener('click', () => {
+    selectedTimeZoneDifference = 10.5; // IST 
+    currentTime();
+});
+
+setInterval(currentTime, 1000);
+
+currentTime();
+
+
 
